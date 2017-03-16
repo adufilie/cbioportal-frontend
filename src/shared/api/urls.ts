ba7004100407a2f704634bac560ecce9400ba5c6
Andy Dufilie <andy.dufilie@gmail.com>
2017-03-16 12:35:42 -0400

    query params loading from url and saving in history

--- src/shared/api/urls.ts
+++ src/shared/api/urls.ts
@@ -1,9 +1,16 @@
 import AppConfig from "appConfig";
-import {default as buildUrl, QueryParams} from 'build-url';
+import {buildUrl, QueryParams} from 'build-url';
 
-function url(path:string, queryParams?:QueryParams, hash?:string) {
-    return buildUrl(`${window.location.protocol}//${AppConfig.host}`, {path, queryParams, hash});
+export type BuildUrlParams = {path:string, queryParams?:QueryParams, hash?:string};
+
+export function buildCBioPortalUrl(params:BuildUrlParams):string;
+export function buildCBioPortalUrl(path:string, queryParams?:QueryParams, hash?:string):string;
+export function buildCBioPortalUrl(pathOrParams:string | BuildUrlParams, queryParams?:QueryParams, hash?:string) {
+    let params = typeof pathOrParams === 'string' ? {path: pathOrParams, queryParams, hash} : pathOrParams;
+    return buildUrl(`${window.location.protocol}//${AppConfig.host}`, params);
 }
+
+const url = buildCBioPortalUrl;
 
 export function getCbioPortalApiUrl() {
     return url('api');
@@ -13,50 +20,6 @@ export function getStudyViewUrl(studyId:string) {
 }
 export function getStudySummaryUrl(studyId:string) {
     return url('study', {id: studyId}, 'summary');
-}
-
-type SubmitQueryUrlParams = {
-    cancer_study_list: ReadonlyArray<string>,
-    cancer_study_id: string,
-    genetic_profile_ids_PROFILE_MUTATION_EXTENDED: string,
-    genetic_profile_ids_PROFILE_COPY_NUMBER_ALTERATION: string,
-    genetic_profile_ids_PROFILE_MRNA_EXPRESSION: string,
-    genetic_profile_ids_PROFILE_METHYLATION: string,
-    genetic_profile_ids_PROFILE_PROTEIN_EXPRESSION: string,
-    Z_SCORE_THRESHOLD: string,
-    RPPA_SCORE_THRESHOLD: string,
-    data_priority: '0'|'1'|'2',
-    case_set_id: string,
-    case_ids: string,
-    patient_case_select: 'sample'|'patient',
-    gene_set_choice: 'user-defined-list',
-    gene_list: string,
-    clinical_param_selection: '',
-    tab_index: 'tab_download'|'tab_visualize',
-    Action: 'Submit',
-};
-export function getSubmitQueryUrl(params:SubmitQueryUrlParams) {
-    if (!params.gene_list)
-        params = {...params, gene_list: ' '};
-
-    if (params.cancer_study_list.length > 1)
-        return url(
-            'cross_cancer.do',
-            {
-                ...params,
-                cancer_study_list: '',
-                cancer_study_id: 'all',
-            },
-            `crosscancer/overview/${
-                params.data_priority
-            }/${
-                encodeURIComponent(params.gene_list)
-            }/${
-                encodeURIComponent(params.cancer_study_list.join(','))
-            }`
-        );
-
-    return url('index.do', params);
 }
 
 export function getPubMedUrl(pmid:string) {
